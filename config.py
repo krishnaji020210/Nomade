b1c6777f1fbee7a8bd6ea6aad569af376a9f6b65
krishnaji020210 <krishnaji020210@gmail.com>
2026-01-06 10:24:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # ============================================================
 #Group Manager Bot
 # Author: LearningBotsOfficial (https://github.com/LearningBotsOfficial) 
-# Support: https://t.me/LearningBotsCommunity
-# Channel: https://t.me/learning_bots
-# YouTube: https://youtube.com/@learning_bots
+# Support: https://t.me/+t42KngFTf5A2YzEx
+# Channel: https://t.me/l_ll_Lord_Krishna_ll_l
+# YouTube: https://youtube.com/@kanhajikideewani
 # License: Open-source (keep credits, no resale)
 # ============================================================
 
@@ -17,10 +17,10 @@ MONGO_URI = os.getenv("MONGO_URI", "")
 DB_NAME = os.getenv("DB_NAME", "Cluster0")
 
 # Owner and bot details
-OWNER_ID = int(os.getenv("5430569954", 0))
+OWNER_ID = int(os.getenv("@l_ll_KRISHNA_ll_l_l", 0))
 BOT_USERNAME = os.getenv("@The_Lord_Sasuke_assistant_bot", "NomadeHelpBot")
 
 # Links and visuals
 SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/+t42KngFTf5A2YzEx")
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/jwowjwug3")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/l_ll_Lord_Krishna_ll_l")
 START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/e8nobe.jpg")
